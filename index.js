06d9a739261489f64dbdbf994932bfdda9d757cf
JonathanNguye15 <4106964-JonathanNguye15@users.noreply.replit.com>
2023-08-23 22:03:33 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+function L(){
+  console.log("L");
+  console.log("L");
+  console.log("L");
+  console.log("L");
+  console.log("L");
+  console.log("L");
+  console.log("L");
+  console.log("LLLLLLLLLLL");
+  console.log("")
+}
+
+function H(){
+  console.log("H        H")
+  console.log("H        H")
+  console.log("H        H")
+  console.log("HHHHHHHHHH")
+  console.log("H        H")
+  console.log("H        H")
+  console.log("H        H")
+  console.log("H        H")
+  console.log("")
+}
+
+function S(){
+  console.log("SSSSSSSSSSSS");
+  console.log("S");
+  console.log("S");
+  console.log("SSSSSSSSSSSS");
+  console.log("           S");
+  console.log("           S");
+  console.log("SSSSSSSSSSSS");
+}
+
+function space(){
+  console.log("")
+  console.log("")
+  console.log("")
+  console.log("")
+  console.log("")
+  console.log("")
+  console.log("")
+  console.log("")
+  console.log("")
+}
+
+function LHS() {
+  L();
+  H();
+  S();
+  space();
+}
+
+for (var a=0; a < 200; a++) {
+console.log(a);
+  LHS()
+  }
